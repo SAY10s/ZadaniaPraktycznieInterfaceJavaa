b71c2241912a4239a5509b07033827ea88d3b21e
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-03-27 16:54:28 +0200

    ADD: models_vehicle

---
+++ src/models_vehicle.java
@@ -0,0 +1,33 @@
+public abstract class models_vehicle {
+    private int speed;
+    private String model;
+
+    public models_vehicle(int speed, String model){
+        this.model = model;
+        this.speed = speed;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString() {
+        return "models_vehicle{" +
+                "speed=" + speed +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+}
